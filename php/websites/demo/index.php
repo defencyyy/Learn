4bac2674447830c752a4222fe99a629463662958
Cyrus <cysness@gmail.com>
2025-02-13 13:41:38 +0800

    Update index.php

--- php/websites/demo/index.php
+++ php/websites/demo/index.php
@@ -18,19 +18,45 @@
           [
             'name' => 'Cy',
             'author' => 'Snowy',
+            'year' => '2021',
             'contact' => '021393213213'
           ],
           [
-            'name' => 'Cy',
+            'name' => 'LOL',
+            'author' => 'Cyka',
+            'year' => '2021',
+            'contact' => '021393213213'
+          ],
+          [
+            'name' => 'What!!',
             'author' => 'Snowy',
+            'year' => '2002',
             'contact' => '021393213213'
           ],
         ];
+
+        //filterAuthor();
+        function filterAuthor($books) {
+          $filteredBooks = [];
+
+          foreach ($books as $book) {
+            if ($book['author'] == 'Snowy') {
+              $filteredBooks[] = $book;
+            }
+          }
+
+          return $filteredBooks;
+        }
       ?>
     <h1>
-      <?php foreach ($books as $book) : ?>
-        <li><?= $book['name'] ?></li>
-      <?php endforeach ?>
+      <?php foreach (filterAuthor($books) as $book) : ?>
+        <li>
+            Title: <?= ($book['name']) ?> <br>
+            Author: <?= ($book['author']) ?> <br>
+            Year: <?= ($book['year']) ?>
+        </li>
+    <?php endforeach; ?>
     </h1>
+
   </body>
 </html>
